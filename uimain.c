0f0e397c24721bae538abc56c08a595adbe22dd6
albertoamdl <aamiranda4@miners.utep.edu>
2022-02-08 13:31:35 -0700

    updated_lab0

--- uimain.c
+++ uimain.c
@@ -28,6 +28,8 @@ int main()
       puts("You selected chars:");
       for (char c = 'A'; c < 'D'; c++)
 	print_char_11x16(c);
+      for(char c = 'A'; c < 'D'; c++)
+	print_char_8x12(c);
       break;
     case 'a':
       puts("you selected arrow!");
